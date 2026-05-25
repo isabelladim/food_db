ee879a44c53deaa2ae75730f6dbeba135d169978
Isabella D <284634042+isabelladim@users.noreply.github.com>
2026-05-25 19:49:03 +1000

    add dbt sources and first staging model for ausnut food nutrients

---
+++ food_db_project/models/staging/stg_ausnut_food_nutrients.sql
@@ -0,0 +1,22 @@
+with 
+source as (
+    select * from {{{{ source('raw','food nutrient profiles') }}}}
+), 
+
+renamed as (
+    select 
+        "Survey ID" as survey_id,
+        "Food name" as food_name,
+        "Energy without dietary fibre (kJ)" as energy_without_dietary_fibre_kj,
+        "Protein (g)" as protein_g,
+        "Total fat (g)" as total_fat_g,
+        "Available carbohydrate (g), without sugar alcohols" as available_carbohydrate_g_without_sugar_alcohols,
+        "Total sugar (g)" as total_sugar_g,
+        "Total dietary fibre (g)" as total_dietary_fibre_g,
+        "Iron (Fe) (mg)" as iron_fe_mg,
+        "Calcium (Ca) (mg)" as calcium_ca_mg
+    from source
+)
+select * from renamed
+
+
