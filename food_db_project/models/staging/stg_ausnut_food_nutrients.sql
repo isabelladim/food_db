22388f1d8c24aeceac218c0f5882e1375eb61843
Isabella D <284634042+isabelladim@users.noreply.github.com>
2026-05-25 20:14:19 +1000

    add first working staging model for ausnut food nutrients

--- food_db_project/models/staging/stg_ausnut_food_nutrients.sql
+++ food_db_project/models/staging/stg_ausnut_food_nutrients.sql
@@ -10,9 +10,9 @@ renamed as (
         "Energy without dietary fibre (kJ)" as energy_without_dietary_fibre_kj,
         "Protein (g)" as protein_g,
         "Total fat (g)" as total_fat_g,
-        "Available carbohydrate (g), without sugar alcohols" as available_carbohydrate_g_without_sugar_alcohols,
-        "Total sugar (g)" as total_sugar_g,
-        "Total dietary fibre (g)" as total_dietary_fibre_g,
+        "Available carbohydrate, without sugar alcohols (g)" as available_carbohydrate_g_without_sugar_alcohols,
+        "Total sugars (g)" as total_sugar_g,
+        "Dietary fibre (g)" as total_dietary_fibre_g,
         "Iron (Fe) (mg)" as iron_fe_mg,
         "Calcium (Ca) (mg)" as calcium_ca_mg
     from source
